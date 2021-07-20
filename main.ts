544ff6d6417282ace46916fe435579e48d4d5dee
lalbury <lester.albury@gmail.com>
2021-07-20 11:56:30 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let item = false
+basic.forever(function () {
+    if (maqueen.sensor(PingUnit.Centimeters) < 35 && maqueen.sensor(PingUnit.Centimeters) != 0) {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CCW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CCW, 150)
+        item = Math.randomBoolean()
+        if (item == true) {
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+            basic.pause(800)
+        }
+        if (item == false) {
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+            basic.pause(800)
+        }
+    } else {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+    }
+})
